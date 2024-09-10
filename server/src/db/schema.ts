0c09ac158a1aa76d7e814006b57df537133565a0
Chitolina <lucas.chitolina01@gmail.com>
2024-09-10 07:38:40 -0300

    initial commit with project dependencies setup and db config :tada:

---
+++ server/src/db/schema.ts
@@ -0,0 +1,12 @@
+import { pgTable, text, integer, timestamp } from 'drizzle-orm/pg-core'
+
+export const goals = pgTable('goals', {
+  id: text('id').primaryKey(),
+  title: text('title').notNull(),
+  desiredWeeklyFrequency: integer('desired_weekly_frequenc y').notNull(),
+  createdAt: timestamp('created_at', {
+    withTimezone: true,
+  })
+    .notNull()
+    .defaultNow(),
+})
---
+++ server/src/http/server.ts
@@ -0,0 +1,13 @@
+import fastify from 'fastify'
+
+const app = fastify()
+
+const port = 3333
+
+app
+  .listen({
+    port,
+  })
+  .then(() => {
+    console.log(`"Server running on port ${port}`)
+  })
